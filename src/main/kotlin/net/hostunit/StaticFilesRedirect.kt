bbd574772267c55dcb400a7c670e903eaffc2603
gq97a6 <szymon.kogut@protonmail.com>
2024-10-21 14:46:13 +0200

    Removed static svg images redirect.

--- src/main/kotlin/net/hostunit/StaticFilesRedirect.kt
+++ src/main/kotlin/net/hostunit/StaticFilesRedirect.kt
@@ -11,13 +11,17 @@ import jakarta.ws.rs.ext.Provider
 @PreMatching
 class StaticFilesRedirect : ContainerRequestFilter {
     override fun filter(context: ContainerRequestContext) {
+        println(context.uriInfo.requestUri.path)
         if (context.uriInfo.requestUri.path.startsWith("/api")) return
+        println(1)
         if (context.uriInfo.requestUri.path.count { it == '/' } <= 1) return
+        println(2)
 
         val requestUri = context.uriInfo.requestUri
         val filename = requestUri.path.substringAfterLast("/")
 
         if (isStaticFile(filename)) {
+            println(3)
             val newUri = UriBuilder.fromUri(requestUri).replacePath("/$filename").build()
             context.abortWith(Response.seeOther(newUri).build())
         }
@@ -30,7 +34,6 @@ class StaticFilesRedirect : ContainerRequestFilter {
             ".js",
             ".map",
             ".html",
-            ".svg",
             ".css",
         ).any { path.endsWith(it) }
     }
